9741246653b837d3e27421ea82fad3c08d35f665
Lee Maskell <lmaskellwebdev@gmail.com>
2021-09-22 13:47:29 +1000

    Additional summary display added

--- src/components/presentational/CoinSummary.js
+++ src/components/presentational/CoinSummary.js
@@ -1,29 +1,49 @@
 import React from 'react'
 import Context from '../container/CoinContext';
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import cs from '../../styles/CoinSummary.module.css';
 
 const CoinSummary = ({ coinData }) => {
     const coinContext = useContext(Context);
+    const [display, setDisplay] = useState(false);
     return (
         <div className={cs.container}>
-            <div className={cs.row}>
-                <div className={cs.coinType}>
-                    <img src={coinData.image} alt="crytpto logo" />
-                    <h4>{coinData.name}</h4>
-                </div>
-                <div className={cs.priceFlex}>
-                    {/* <p className={cs.symbol}>{coinData.symbol}</p> */}
-                    <p className={cs.price}>${coinData.current_price}</p>
-                </div>
-                <div className={cs.percentageFlex}>
-                    {coinData.price_change_percentage_24h < 0 ? (
-                        <p className={`${cs.percent} ${cs.red}`}>{coinData.price_change_percentage_24h.toFixed(2)}%</p>
-                    ) : (<p className={`${cs.percent} ${cs.green}`}>{coinData.price_change_percentage_24h.toFixed(2)}%</p>)}
-                </div>
-                <div className={cs.buttonFlex}>
-                    <button className={cs.btn} onClick={() => coinContext.addToWatchlist(coinData)}>+</button>
-                </div>
+            <div className={cs.contentFlex}>
+                <section className={cs.row}>
+                    <div className={cs.coinType}>
+                        <img src={coinData.image} alt="crytpto logo" />
+                        <h4 onClick={() => setDisplay(true)}>{coinData.name}</h4>
+                    </div>
+                    <div className={cs.priceFlex}>
+                        {/* <p className={cs.symbol}>{coinData.symbol}</p> */}
+                        <p className={cs.price}>${coinData.current_price}</p>
+                    </div>
+                    <div className={cs.percentageFlex}>
+                        {coinData.price_change_percentage_24h < 0 ? (
+                            <p className={`${cs.percent} ${cs.red}`}>{coinData.price_change_percentage_24h.toFixed(2)}%</p>
+                        ) : (<p className={`${cs.percent} ${cs.green}`}>{coinData.price_change_percentage_24h.toFixed(2)}%</p>)}
+                    </div>
+                    <div className={cs.buttonFlex}>
+                        <button className={cs.btn} onClick={() => coinContext.addToWatchlist(coinData)}>+</button>
+                    </div>
+                </section>
+                <section className={display ? `${cs.row}` : `${cs.noRow}`}>
+                    <div className={cs.closeBtn}>
+                        <h2 onClick={() => setDisplay(false)}>x</h2>
+                    </div>
+                    <div className={cs.displayFlex}>
+                        <p>Variation: ${coinData.price_change_24h.toFixed(2)}</p>
+                    </div>
+                    <div className={cs.displayFlex}>
+                        <p>24hr high: ${coinData.high_24h.toFixed(2)}</p>
+                    </div>
+                    <div className={cs.displayFlex}>
+                        <p>24hr low: ${coinData.low_24h.toFixed(2)}</p>
+                    </div>
+                    <div className={cs.mktCapConatainer}>
+                        <p>Mkt Cap: ${coinData.market_cap.toLocaleString()}</p>
+                    </div>
+                </section>
             </div>
         </div>
     )
